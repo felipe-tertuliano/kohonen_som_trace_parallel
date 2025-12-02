034147ff7903aab2a81e8d04708ad2de5d50bed1
Felipe Tertuliano <felipeltertuliano@gmail.com>
2025-12-01 21:51:59 -0300

    chore: move changes

--- openmp_gpu_kohonen_som_trace.c
+++ openmp_gpu_kohonen_som_trace.c
@@ -3,21 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #ifndef max
-/** shorthand for maximum value */
 #define max(a, b) (((a) > (b)) ? (a) : (b))
 #endif
 #ifndef min
-/** shorthand for minimum value */
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
 /**
  * Helper function to generate a random number in a given interval
- * \param a lower limit
- * \param b upper limit
- * \returns random number in the range \f$[a,b)\f$
  */
 double _random(double a, double b)
 {
@@ -26,18 +22,12 @@ double _random(double a, double b)
 }
 
 /**
- * Save a given n-dimensional data martix to file
- * \param [in] fname filename to save in (gets overwriten without confirmation)
- * \param [in] X matrix to save
- * \param [in] num_points rows in the matrix = number of points
- * \param [in] num_features columns in the matrix = dimensions of points
- * \returns 0 if all ok
- * \returns -1 if file creation failed
+ * Save a given n-dimensional data matrix to file
  */
 int save_nd_data(const char *fname, double **X, int num_points, int num_features)
 {
     FILE *fp = fopen(fname, "wt");
-    if (!fp)  // error with fopen
+    if (!fp)
     {
         char msg[120];
         sprintf(msg, "File error (%s): ", fname);
@@ -45,35 +35,30 @@ int save_nd_data(const char *fname, double **X, int num_points, int num_features
         return -1;
     }
 
-    for (int i = 0; i < num_points; i++)  // for each point in the array
+    for (int i = 0; i < num_points; i++)
     {
-        for (int j = 0; j < num_features; j++)  // for each feature in the array
+        for (int j = 0; j < num_features; j++)
         {
-            fprintf(fp, "%.4g", X[i][j]);  // print the feature value
-            if (j < num_features - 1)      // if not the last feature
-                fprintf(fp, ",");          // suffix comma
+            fprintf(fp, "%.4g", X[i][j]);
+            if (j < num_features - 1)
+                fprintf(fp, ",");
         }
-        if (i < num_points - 1)  // if not the last row
-            fprintf(fp, "\n");   // start a new line
+        if (i < num_points - 1)
+            fprintf(fp, "\n");
     }
     fclose(fp);
     return 0;
 }
 
 /**
- * Get minimum value and index of the value in a vector
- * \param[in] X vector to search
- * \param[in] N number of points in the vector
- * \param[out] val minimum value found
- * \param[out] idx index where minimum value was found
+ * Get minimum value and index in a vector (CPU version for reduction)
  */
 void kohonen_get_min_1d(double const *X, int N, double *val, int *idx)
 {
-    val[0] = INFINITY;  // initial min value
-
-    for (int i = 0; i < N; i++)  // check each value
+    val[0] = INFINITY;
+    for (int i = 0; i < N; i++)
     {
-        if (X[i] < val[0])  // if a lower value is found save the value and its index
+        if (X[i] < val[0])
         {
             idx[0] = i;
             val[0] = X[i];
@@ -82,74 +67,79 @@ void kohonen_get_min_1d(double const *X, int N, double *val, int *idx)
 }
 
 /**
- * Update weights of the SOM using Kohonen algorithm
- * \param[in] x data point
- * \param[in,out] W weights matrix
- * \param[in,out] D temporary vector to store distances
- * \param[in] num_out number of output points
- * \param[in] num_features number of features per input sample
- * \param[in] alpha learning rate \f$0<\alpha\le1\f$
- * \param[in] R neighborhood range
+ * Update weights using GPU offloading with OpenMP
  */
-void kohonen_update_weights(double const *x, double *const *W, double *D, int num_out, int num_features, double alpha, int R)
+void kohonen_update_weights(double const *x, double **W, double *D, 
+                           int num_out, int num_features, double alpha, int R)
 {
     int j, k;
+    
+    // Flatten W for GPU transfer (create 1D view)
+    double *W_flat = (double *)malloc(num_out * num_features * sizeof(double));
+    for (int i = 0; i < num_out; i++)
+        for (int f = 0; f < num_features; f++)
+            W_flat[i * num_features + f] = W[i][f];
 
-    // step 1: for each output point
+    // Step 1: Compute distances on GPU
+    #pragma omp target teams distribute parallel for map(to: x[0:num_features], W_flat[0:num_out*num_features]) map(from: D[0:num_out])
     for (j = 0; j < num_out; j++)
     {
-        D[j] = 0.f;
-        // compute Euclidian distance of each output
-        // point from the current sample
+        D[j] = 0.0;
         for (k = 0; k < num_features; k++)
-            D[j] += (W[j][k] - x[k]) * (W[j][k] - x[k]);
+        {
+            double diff = W_flat[j * num_features + k] - x[k];
+            D[j] += diff * diff;
+        }
     }
 
-    // step 2:  get closest node i.e., node with smallest Euclidian distance to
-    // the current pattern
+    // Step 2: Find minimum (on CPU for simplicity with small num_out)
     int d_min_idx;
     double d_min;
     kohonen_get_min_1d(D, num_out, &d_min, &d_min_idx);
 
-    // step 3a: get the neighborhood range
+    // Step 3: Update weights in neighborhood on GPU
     int from_node = max(0, d_min_idx - R);
     int to_node = min(num_out, d_min_idx + R + 1);
+    int range = to_node - from_node;
 
-    // step 3b: update the weights of nodes in the
-    // neighborhood
-    for (j = from_node; j < to_node; j++)
+    #pragma omp target teams distribute parallel for collapse(2) map(tofrom: W_flat[from_node*num_features:(to_node-from_node)*num_features]) map(to: x[0:num_features])
+    for (j = 0; j < range; j++)
+    {
         for (k = 0; k < num_features; k++)
-            // update weights of nodes in the neighborhood
-            W[j][k] += alpha * (x[k] - W[j][k]);
+        {
+            int actual_j = from_node + j;
+            int idx = actual_j * num_features + k;
+            W_flat[idx] += alpha * (x[k] - W_flat[idx]);
+        }
+    }
+
+    // Copy back to 2D structure
+    for (int i = 0; i < num_out; i++)
+        for (int f = 0; f < num_features; f++)
+            W[i][f] = W_flat[i * num_features + f];
+    
+    free(W_flat);
 }
 
 /**
- * Apply incremental algorithm with updating neighborhood and learning rates on all samples in the given datset
- * \param[in] X data set
- * \param[in,out] W weights matrix
- * \param[in] num_samples number of output points
- * \param[in] num_features number of features per input sample
- * \param[in] num_out number of output points
- * \param[in] alpha_min terminal value of alpha
+ * Apply SOM training algorithm with GPU acceleration
  */
-void kohonen_som_tracer(double **X, double *const *W, int num_samples, int num_features, int num_out, double alpha_min)
+void kohonen_som_tracer(double **X, double **W, int num_samples, 
+                       int num_features, int num_out, double alpha_min)
 {
     int R = num_out >> 2, iter = 0;
     double alpha = 1.f;
     double *D = (double *)malloc(num_out * sizeof(double));
 
-    // Loop alpha from 1 to alpha_min
+    // Main training loop
     for (; alpha > alpha_min; alpha -= 0.01, iter++)
     {
-        // Loop for each sample pattern in the data set
         for (int sample = 0; sample < num_samples; sample++)
         {
             const double *x = X[sample];
-            // update weights for the current input pattern sample
             kohonen_update_weights(x, W, D, num_out, num_features, alpha, R);
         }
 
-        // every 10th iteration, reduce the neighborhood range
         if (iter % 10 == 0 && R > 1)
             R--;
     }
@@ -157,22 +147,20 @@ void kohonen_som_tracer(double **X, double *const *W, int num_samples, int num_f
     free(D);
 }
 
-/** Creates a random set of points distributed *near* the circumference of a circle and trains an SOM that finds that circular pattern
- * \param[out] data matrix to store data in
- * \param[in] N number of points required
+/**
+ * Creates random points near circle circumference
  */
 void test_circle(double *const *data, int N)
 {
     const double R = 0.75, dr = 0.3;
-    double a_t = 0., b_t = 2.f * M_PI;  // theta random between 0 and 2*pi
-    double a_r = R - dr, b_r = R + dr;  // radius random between R-dr and R+dr
-    int i;
+    double a_t = 0., b_t = 2.f * M_PI;
+    double a_r = R - dr, b_r = R + dr;
 
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        double r = _random(a_r, b_r);      // random radius
-        double theta = _random(a_t, b_t);  // random theta
-        data[i][0] = r * cos(theta);       // convert from polar to cartesian
+        double r = _random(a_r, b_r);
+        double theta = _random(a_t, b_t);
+        data[i][0] = r * cos(theta);
         data[i][1] = r * sin(theta);
     }
 }
@@ -183,49 +171,45 @@ void test1()
     int features = 2;
     int num_out = 50;
 
-    // 2D space, hence size = number of rows * 2
     double **X = (double **)malloc(N * sizeof(double *));
-
-    // number of clusters nodes * 2
     double **W = (double **)malloc(num_out * sizeof(double *));
-
-    for (int i = 0; i < max(num_out, N); i++)  // loop till max(N, num_out)
-    {
-        if (i < N)  // only add new arrays if i < N
-            X[i] = (double *)malloc(features * sizeof(double));
-        if (i < num_out)  // only add new arrays if i < num_out
-        {
-            W[i] = (double *)malloc(features * sizeof(double));
-            // preallocate with random initial weights
-            for (j = 0; j < features; j++) W[i][j] = _random(-1, 1);
-        }
-    }
-
-    test_circle(X, N);  // create test data around circumference of a circle
-    save_nd_data("test1.csv", X, N, features);  // save test data points
-    save_nd_data("w11.csv", W, num_out, features);  // save initial random weights
-    kohonen_som_tracer(X, W, N, features, num_out, 0.1);  // train the SOM
-    save_nd_data("w12.csv", W, num_out, features);  // save the resultant weights
 
     for (int i = 0; i < max(num_out, N); i++)
     {
         if (i < N)
-            free(X[i]);
+            X[i] = (double *)malloc(features * sizeof(double));
         if (i < num_out)
-            free(W[i]);
+        {
+            W[i] = (double *)malloc(features * sizeof(double));
+            for (j = 0; j < features; j++) 
+                W[i][j] = _random(-1, 1);
+        }
     }
+
+    test_circle(X, N);
+    save_nd_data("test1.csv", X, N, features);
+    save_nd_data("w11.csv", W, num_out, features);
+    kohonen_som_tracer(X, W, N, features, num_out, 0.1);
+    save_nd_data("w12.csv", W, num_out, features);
+
+    for (int i = 0; i < max(num_out, N); i++)
+    {
+        if (i < N) free(X[i]);
+        if (i < num_out) free(W[i]);
+    }
+    free(X);
+    free(W);
 }
 
 void test_lamniscate(double *const *data, int N)
 {
     const double dr = 0.2;
-    int i;
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        double dx = _random(-dr, dr);     // random change in x
-        double dy = _random(-dr, dr);     // random change in y
-        double theta = _random(0, M_PI);  // random theta
-        data[i][0] = dx + cos(theta);     // convert from polar to cartesian
+        double dx = _random(-dr, dr);
+        double dy = _random(-dr, dr);
+        double theta = _random(0, M_PI);
+        data[i][0] = dx + cos(theta);
         data[i][1] = dy + sin(2. * theta) / 2.f;
     }
 }
@@ -237,31 +221,29 @@ void test2()
     int num_out = 20;
     double **X = (double **)malloc(N * sizeof(double *));
     double **W = (double **)malloc(num_out * sizeof(double *));
-    for (int i = 0; i < max(num_out, N); i++)
-    {
-        if (i < N)  // only add new arrays if i < N
-            X[i] = (double *)malloc(features * sizeof(double));
-        if (i < num_out)  // only add new arrays if i < num_out
-        {
-            W[i] = (double *)malloc(features * sizeof(double));
-
-            // preallocate with random initial weights
-            for (j = 0; j < features; j++) W[i][j] = _random(-1, 1);
-        }
-    }
-
-    test_lamniscate(X, N);  // create test data around the lamniscate
-    save_nd_data("test2.csv", X, N, features);  // save test data points
-    save_nd_data("w21.csv", W, num_out, features);  // save initial random weights
-    kohonen_som_tracer(X, W, N, features, num_out, 0.01);  // train the SOM
-    save_nd_data("w22.csv", W, num_out, features);  // save the resultant weights
 
     for (int i = 0; i < max(num_out, N); i++)
     {
         if (i < N)
-            free(X[i]);
+            X[i] = (double *)malloc(features * sizeof(double));
         if (i < num_out)
-            free(W[i]);
+        {
+            W[i] = (double *)malloc(features * sizeof(double));
+            for (j = 0; j < features; j++) 
+                W[i][j] = _random(-1, 1);
+        }
+    }
+
+    test_lamniscate(X, N);
+    save_nd_data("test2.csv", X, N, features);
+    save_nd_data("w21.csv", W, num_out, features);
+    kohonen_som_tracer(X, W, N, features, num_out, 0.01);
+    save_nd_data("w22.csv", W, num_out, features);
+
+    for (int i = 0; i < max(num_out, N); i++)
+    {
+        if (i < N) free(X[i]);
+        if (i < num_out) free(W[i]);
     }
     free(X);
     free(W);
@@ -269,22 +251,18 @@ void test2()
 
 void test_3d_classes(double *const *data, int N)
 {
-    const double R = 0.1;  // radius of cluster
-    int i;
+    const double R = 0.1;
     const int num_classes = 4;
     const double centres[][3] = {
-        // centres of each class cluster
-        {.5, .5, .5},    // centre of class 1
-        {.5, -.5, -.5},  // centre of class 2
-        {-.5, .5, .5},   // centre of class 3
-        {-.5, -.5 - .5}  // centre of class 4
+        {.5, .5, .5},
+        {.5, -.5, -.5},
+        {-.5, .5, .5},
+        {-.5, -.5, -.5}
     };
 
-    for (i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        int class = rand() % num_classes;  // select a random class for the point
-
-        // create random coordinates (x,y,z) around the centre of the class
+        int class = rand() % num_classes;
         data[i][0] = _random(centres[class][0] - R, centres[class][0] + R);
         data[i][1] = _random(centres[class][1] - R, centres[class][1] + R);
         data[i][2] = _random(centres[class][2] - R, centres[class][2] + R);
@@ -298,61 +276,60 @@ void test3()
     int num_out = 20;
     double **X = (double **)malloc(N * sizeof(double *));
     double **W = (double **)malloc(num_out * sizeof(double *));
-    for (int i = 0; i < max(num_out, N); i++)
-    {
-        if (i < N)  // only add new arrays if i < N
-            X[i] = (double *)malloc(features * sizeof(double));
-        if (i < num_out)  // only add new arrays if i < num_out
-        {
-            W[i] = (double *)malloc(features * sizeof(double));
-            
-            // preallocate with random initial weights
-            for (j = 0; j < features; j++) W[i][j] = _random(-1, 1);
-        }
-    }
-
-    test_3d_classes(X, N);  // create test data around the lamniscate
-    save_nd_data("test3.csv", X, N, features);  // save test data points
-    save_nd_data("w31.csv", W, num_out, features);  // save initial random weights
-    kohonen_som_tracer(X, W, N, features, num_out, 0.01);  // train the SOM
-    save_nd_data("w32.csv", W, num_out, features);  // save the resultant weights
 
     for (int i = 0; i < max(num_out, N); i++)
     {
         if (i < N)
-            free(X[i]);
+            X[i] = (double *)malloc(features * sizeof(double));
         if (i < num_out)
-            free(W[i]);
+        {
+            W[i] = (double *)malloc(features * sizeof(double));
+            for (j = 0; j < features; j++) 
+                W[i][j] = _random(-1, 1);
+        }
+    }
+
+    test_3d_classes(X, N);
+    save_nd_data("test3.csv", X, N, features);
+    save_nd_data("w31.csv", W, num_out, features);
+    kohonen_som_tracer(X, W, N, features, num_out, 0.01);
+    save_nd_data("w32.csv", W, num_out, features);
+
+    for (int i = 0; i < max(num_out, N); i++)
+    {
+        if (i < N) free(X[i]);
+        if (i < num_out) free(W[i]);
     }
     free(X);
     free(W);
 }
 
-/**
- * Convert clock cycle difference to time in seconds
- * \param[in] start_t start clock
- * \param[in] end_t end clock
- * \returns time difference in seconds
- */
 double get_clock_diff(clock_t start_t, clock_t end_t)
 {
     return (double)(end_t - start_t) / (double)CLOCKS_PER_SEC;
 }
 
-/** Main function */
 int main(int argc, char **argv)
 {
+    srand(time(NULL));
+    
+    printf("Running GPU-accelerated Kohonen SOM tests...\n");
+    printf("Available devices: %d\n", omp_get_num_devices());
+    
     clock_t start_clk = clock();
     test1();
     clock_t end_clk = clock();
     printf("Test 1 completed in %.4g sec\n", get_clock_diff(start_clk, end_clk));
+    
     start_clk = clock();
     test2();
     end_clk = clock();
     printf("Test 2 completed in %.4g sec\n", get_clock_diff(start_clk, end_clk));
+    
     start_clk = clock();
     test3();
     end_clk = clock();
     printf("Test 3 completed in %.4g sec\n", get_clock_diff(start_clk, end_clk));
+    
     return 0;
 }
